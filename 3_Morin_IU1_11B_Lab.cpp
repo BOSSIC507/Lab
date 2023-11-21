86fa0f8237aafd5b000d3e17a0aaf751e27038ea
BOSSIC507 <98617944+BOSSIC507@users.noreply.github.com>
2023-11-21 14:47:35 +0300

    Add files via upload

---
+++ 3_Morin_IU1_11B_Lab.cpp
@@ -0,0 +1,162 @@
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct Product{ //создаем структуру для блюд, компоненты -- название, тип и цена 
+    string name;
+    string type1;
+    double price;
+    
+};
+
+
+void addProductToFile(const Product& product) { //функция добавления блюда в файл
+    ofstream outFile("products.txt", ios::app); //ios::app -- запись происходит в конец файла
+    if (outFile.is_open()) {
+        outFile << product.name << "," << product.type1 << "," << product.price << endl;
+        outFile.close();
+        cout << "Блюдо успешно добавлен." << endl;
+    } else {
+        cerr << "Не удалось открыть файл для записи." << endl;
+    }
+}
+
+
+void searchProductByPrice(const double& maxPrice) { //функция поиска блюда по цене
+    ifstream inFile("products.txt");
+    if (inFile.is_open()) {
+        string line;
+        bool found = false;
+        while (getline(inFile, line)) {
+            size_t pos1 = line.find_first_of(',');
+            size_t pos2 = line.find_last_of(',');
+            double thatPrice = stod(line.substr(pos1 + 1, pos2 - pos1));
+            if (thatPrice <= maxPrice) {
+                cout << "Найдены продукты: " << line << endl;
+                found = true;
+            }
+        }
+        inFile.close();
+        if (!found) {
+            cout << "Блюда, цена которых меньше заданной, не найдены." << endl;
+        }
+    } else {
+        cerr << "Не удалось открыть файл для чтения." << endl;
+    }
+}
+
+
+void sortProductbyNumber() {
+    ifstream inFile("products.txt");
+    if (inFile.is_open()) {
+        // Чтение данных и сортировка (пузырьковая сортировка)
+        string lines[100]; // Предполагаем, что не более 100 блюд
+        int count = 0;
+        string line;
+
+        while (getline(inFile, line)) {
+            lines[count] = line;
+            count++;
+        }
+        inFile.close();
+
+        for (int i = 0; i < count - 1; i++) {
+            for (int j = 0; j < count - i - 1; j++) {
+                // преобразовываем конец строки(количество) из string в int для сравнения
+                size_t pos1 = lines[j].find_last_of(','); //find_last_of() позволяет найти индекс последнего символа "," в строке:
+                size_t pos2 = lines[j + 1].find_last_of(',');
+
+                int Par1 = stoi(lines[j].substr(pos1 + 1)); //stoi -- преобразовывает string в int
+                int Par2 = stoi(lines[j + 1].substr(pos2 + 1));
+
+                if (Par1 > Par2) {
+                    swap(lines[j], lines[j + 1]);
+                }
+            }
+        }
+
+        // Запись отсортированных данных обратно в файл
+        ofstream outFile("products.txt");
+        if (outFile.is_open()) {
+            for (int i = 0; i < count; i++) {
+                outFile << lines[i] << endl;
+            }
+            outFile.close();
+            cout << "Блюда отсортированы по количеству." << endl;
+        } else {
+            cerr << "Не удалось открыть файл для записи." << endl;
+        }
+    } else {
+        cerr << "Не удалось открыть файл для чтения." << endl;
+    }
+}
+
+int main() {
+    int choice;
+    double maxPrice;
+
+    do {
+        cout << "Меню:" << endl;
+        cout << "1. Добавить блюдо" << endl;
+        cout << "2. Поиск продуктов по цене" << endl;
+        cout << "3. Сортировать продукты по цене" << endl;
+        cout << "0. Выйти" << endl;
+        cout << "Выберите действие: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                cin.ignore();
+                string name;
+                double price;
+                string type1;
+
+                cout << "Введите название блюда: ";
+                getline(cin, name);
+                if (name.empty()) {
+                    cout << "Ввод прерван." << endl;
+                    break;
+                }
+                cout << "Введите тип: ";
+                getline(cin, type1);
+                if (name.empty()) {
+                    cout << "Ввод прерван." << endl;
+                    break;
+                }
+                cout << "Введите цену: ";
+                cin >> price;
+                
+
+                Product newProduct = {name, type1, price};
+                addProductToFile(newProduct);
+break;
+            }
+            case 2: {
+                cin.ignore();
+                cout << "Введите максимальную цену для поиска: ";
+                cin >> maxPrice;
+                searchProductByPrice(maxPrice);
+                break;
+            }
+            case 3: {
+                sortProductbyNumber();
+                break;
+            }
+            
+        
+            case 0: {
+                cout << "Программа завершена." << endl;
+                break;
+            }
+            default: {
+                cout << "Неверный выбор. Попробуйте снова." << endl;
+                break;
+            }
+        }
+    } while (choice != 0);
+
+    return 0;
+}
